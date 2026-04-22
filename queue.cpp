572a51f09e4ad88d38638809d0dfcdcbb13fdcfc
hafimuhammad <hafimuhammad47@gmail.com>
2026-04-22 17:53:01 +0800

    code

--- queue.cpp
+++ queue.cpp
@@ -14,17 +14,22 @@ bool isFull(const Queue* q) {
 }
 
 void enqueue(Queue* q, int value) {
+    if (isFull(q)) throw std::overflow_error("Queue penuh!");
     q->rear++;
     *(q->rear) = value;
 }
 
 void dequeue(Queue* q) {
+    if (isEmpty(q)) throw std::underflow_error("Queue kosong!");
     q->front++;
 }
 
 int front(const Queue* q) {
+    if (isEmpty(q)) throw std::underflow_error("Queue kosong!");
     return *(q->front);
 }
 
 int back(const Queue* q) {
+    if (isEmpty(q)) throw std::underflow_error("Queue kosong!");
     return *(q->rear);
+}
