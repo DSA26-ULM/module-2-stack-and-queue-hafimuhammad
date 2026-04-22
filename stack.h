5f97dfcdec73b6ec9830ea129722d143aa7b1a49
hafimuhammad <hafimuhammad47@gmail.com>
2026-04-22 15:51:44 +0800

    code

--- stack.h
+++ stack.h
@@ -5,36 +5,16 @@
 
 struct Stack {
     int data[MAX];
-    int* top;
 };
 
-void init(Stack* s) {
-    s->top = s->data - 1;
+
+inline void push(Stack* s, int value) {
+    if (isFull(s)) throw std::overflow_error("Stack penuh!");
+    s->data[++s->top] = value;
 }
 
-bool isEmpty(const Stack* s) {
-    return s->top < s->data;
 }
 
-bool isFull(const Stack* s) {
-    return s->top >= s->data + MAX - 1;
-}
-
-void push(Stack* s, int value) {
-    if (!isFull(s)) {
-        s->top++;
-        *(s->top) = value;
-    }
-}
-
-void pop(Stack* s) {
-    if (!isEmpty(s)) {
-        s->top--;
-    }
-}
-
-int peek(const Stack* s) {
-    return *(s->top);
 }
 
 #endif
