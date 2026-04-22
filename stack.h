dc839acdfb6df1128a4ca2137bb1b8a77661f640
hafimuhammad <hafimuhammad47@gmail.com>
2026-04-22 18:53:20 +0800

    code

--- stack.h
+++ stack.h
@@ -6,8 +6,6 @@
 #define MAX 100
 
 struct Stack {
-    int data[MAX];
-    int top = -1;
     long long data[MAX];
     int top;
 };
@@ -15,10 +13,8 @@ struct Stack {
 void init(Stack* s);
 bool isEmpty(const Stack* s);
 bool isFull(const Stack* s);
-void push(Stack* s, int value);
 void push(Stack* s, long long value);
 void pop(Stack* s);
-int peek(const Stack* s);
 long long peek(const Stack* s);
 
 #endif
