9203cf85132bad159a45e058e45f7b66fd4f4dbe
hafimuhammad <hafimuhammad47@gmail.com>
2026-06-13 16:12:37 +0800

    update
    Refactored main function to use inputData array and HasilOperasi function for calculations.

--- prob1.cpp
+++ prob1.cpp
@@ -1,43 +1,20 @@
-#include <iostream>
-#include <string>
-#include "stack.h"
-
-using namespace std;
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int n;
-    cin >> n;
-
-    Stack st;
-    init(&st);
-
-    for (int i = 0; i < n; i++) {
-        string token;
-        cin >> token;
-
-        if (token == "+" || token == "-" || token == "*" || token == "/") {
-            
-            int b = peek(&st); 
-            pop(&st);
-            int a = peek(&st); 
-            pop(&st);
-
-            int result;
-            if (token == "+") result = a + b;
-            else if (token == "-") result = a - b;
-            else if (token == "*") result = a * b;
-            else result = a / b;
-
-            push(&st, result);
-        } else {
-            push(&st, stoi(token));
-        }
-    }
-
-    cout << peek(&st) << endl;
-
-    return 0;
-}
+1	#include <iostream>
+2	#include <string>
+3	#include "stack.h"
+4	using namespace std;
+5	
+6	int main(){
+7	Stack s;
+8	init(&s);
+9
+10	string inputData[MAX];
+11	int n;
+12	if (!(cin >> n)) return 0;
+13	
+14	for (int i = 0; i < n && i < MAX; i++) {
+15	cin >> inputData[i];
+16	}
+17	int hasil = HasilOperasi(&s, n, inputData);
+18	cout << hasil << endl;
+19	return 0;
+20	}
