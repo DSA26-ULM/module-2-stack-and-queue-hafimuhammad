9879ee65fbffebad8551d53caf754dc05d813fb3
hafimuhammad <hafimuhammad47@gmail.com>
2026-06-13 16:13:54 +0800

    update

--- prob1.cpp
+++ prob1.cpp
@@ -1,20 +1,19 @@
-1	#include <iostream>
-2	#include <string>
-3	#include "stack.h"
-4	using namespace std;
-5	
-6	int main(){
-7	Stack s;
-8	init(&s);
-9
-10	string inputData[MAX];
-11	int n;
-12	if (!(cin >> n)) return 0;
-13	
-14	for (int i = 0; i < n && i < MAX; i++) {
-15	cin >> inputData[i];
-16	}
-17	int hasil = HasilOperasi(&s, n, inputData);
-18	cout << hasil << endl;
-19	return 0;
-20	}
+#include <iostream>
+#include <string>
+#include "stack.h"
+using namespace std;
+
+int main(){
+Stack s;
+init(&s);
+string inputData[MAX];
+int n;
+if (!(cin >> n)) return 0;
+
+for (int i = 0; i < n && i < MAX; i++) {
+cin >> inputData[i];
+}
+int hasil = HasilOperasi(&s, n, inputData);
+cout << hasil << endl;
+return 0;
+}
